db766b719cb63f7287ca2c82cdacbbc0ae4c4b97
RodrigoA15 <rapapamija@misena.edu.co>
2024-04-11 11:16:09 -0500

    mapa final

--- markers.js
+++ markers.js
@@ -242,6 +242,52 @@ export const marker = () => {
           "https://www.google.com/maps/@2.4750613,-76.5799628,3a,90y,40.86h,71.73t/data=!3m7!1e1!3m5!1snK2k0mqi_qhJkzSAbU7Gbw!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DnK2k0mqi_qhJkzSAbU7Gbw%26cb_client%3Dmaps_sv.share%26w%3D900%26h%3D600%26yaw%3D40.86354083120442%26pitch%3D18.267613857652606%26thumbfov%3D90!7i13312!8i6656?authuser=0&coh=205410&entry=ttu";
       })
       .addTo(map);
+
+    L.circleMarker([2.492672471974319, -76.58010759863058], {
+      className: "leaflet-circleMarker-icon",
+      radius: 24,
+      color: "blue",
+      fillColor: "#f03",
+    }).on("click", () => {
+      window.location.href =
+        "https://www.google.com/maps/@2.4927179,-76.5800808,3a,75y,245.89h,88.99t/data=!3m7!1e1!3m5!1s9MAVmjF4_dEufJELjXGd5g!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3D9MAVmjF4_dEufJELjXGd5g%26cb_client%3Dmaps_sv.share%26w%3D900%26h%3D600%26yaw%3D245.89334770443642%26pitch%3D1.0132803951416633%26thumbfov%3D90!7i13312!8i6656?authuser=0&coh=205410&entry=ttu";
+    });
+
+    L.circleMarker([2.492672471974319, -76.58010759863058], {
+      className: "leaflet-circleMarker-icon",
+      radius: 24,
+      color: "blue",
+      fillColor: "#f03",
+    })
+      .on("click", () => {
+        window.location.href =
+          "https://www.google.com/maps/@2.4927179,-76.5800808,3a,75y,245.89h,88.99t/data=!3m7!1e1!3m5!1s9MAVmjF4_dEufJELjXGd5g!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3D9MAVmjF4_dEufJELjXGd5g%26cb_client%3Dmaps_sv.share%26w%3D900%26h%3D600%26yaw%3D245.89334770443642%26pitch%3D1.0132803951416633%26thumbfov%3D90!7i13312!8i6656?authuser=0&coh=205410&entry=ttu";
+      })
+      .addTo(map);
+
+    L.circleMarker([2.4422782789902295, -76.60893859325604], {
+      className: "leaflet-circleMarker-icon",
+      radius: 24,
+      color: "blue",
+      fillColor: "#f03",
+    })
+      .on("click", () => {
+        window.location.href =
+          "https://www.google.com/maps/@2.4422046,-76.6087646,3a,75y,110.78h,77.15t/data=!3m7!1e1!3m5!1sbuWNPveZWQbPi_IRZsziag!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DbuWNPveZWQbPi_IRZsziag%26cb_client%3Dmaps_sv.share%26w%3D900%26h%3D600%26yaw%3D110.78210772541524%26pitch%3D12.85464300879707%26thumbfov%3D90!7i13312!8i6656?authuser=0&coh=205410&entry=ttu";
+      })
+      .addTo(map);
+
+    L.circleMarker([2.449888411906566, -76.63678368265776], {
+      className: "leaflet-circleMarker-icon",
+      radius: 24,
+      color: "blue",
+      fillColor: "#f03",
+    })
+      .on("click", () => {
+        window.location.href =
+          "https://www.google.com/maps/@2.4422046,-76.6087646,3a,75y,110.78h,77.15t/data=!3m7!1e1!3m5!1sbuWNPveZWQbPi_IRZsziag!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DbuWNPveZWQbPi_IRZsziag%26cb_client%3Dmaps_sv.share%26w%3D900%26h%3D600%26yaw%3D110.78210772541524%26pitch%3D12.85464300879707%26thumbfov%3D90!7i13312!8i6656?authuser=0&coh=205410&entry=ttu";
+      })
+      .addTo(map);
   }
 
   function removerHeridos() {
